fd92569bfb598e9c604d058d810e572d00cb6eb2
Marcus Povey <marcus@marcus-povey.co.uk>
2019-04-19 21:51:20 +0100

    Adding page events capture

--- Main.php
+++ Main.php
@@ -2,7 +2,59 @@
 
 namespace IdnoPlugins\Recaptcha3;
 
-class Main extends Idno\Common\Plugin 
-{
+class Main extends Idno\Common\Plugin {
+
+    function registerTranslations() {
+        \Idno\Core\Idno::site()->language()->register(
+                new \Idno\Core\GetTextTranslation(
+                        'recaptcha3', dirname(__FILE__) . '/languages/'
+                )
+        );
+    }
     
+    function pageInterceptEvent(\Idno\Core\Event $event) {
+        
+        $config = self::getConfig();
+        
+        $captcha = new Recaptcha3($config['siteKey'], $config['privateKey']);
+        
+        $captcha->gatekeeper(\Idno\Core\Idno::site()->currentPage()->getInput('recaptcha-action'));
+    }
+    
+    function registerEventHooks() {
+        
+        // Load configuration
+        $config = self::getConfig();
+        
+        if (!empty($config['siteKey']) && !empty($config['privateKey'])) {
+            
+            // Override page hooks
+            
+            //\Idno\Core\Idno::site()->addEventHook('page/get', [$this, 'pageInterceptEvent']);
+            \Idno\Core\Idno::site()->addEventHook('page/post', [$this, 'pageInterceptEvent']);
+            
+        }
+        
+    }
+
+    public static function getConfig() {
+	    $config = \Idno\Core\Idno::site()->config();
+	    if (empty($config->recaptcha3)) {
+		$config->recaptcha3 = [
+                    'siteKey' => '',
+                    'privateKey' => '',
+                    
+                    'thresholds' => [
+                        'annotation/post' => 0.5,
+                        'session/login' => 0.5,
+                        'account/password' => 0.5,
+                        'account/register' => 0.5
+                    ],
+                    
+                    'defaultThreshold' => 0.5
+		];
+	    }
+	    
+	    return $config->recaptcha3;
+	}
 }
